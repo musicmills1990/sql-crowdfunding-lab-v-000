b25b9c3fafccee49aa6283f5f79277f5f7d83afc
Marc Mills <mdmills1990@gmail.com>
2018-10-25 01:01:21 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,5 +53,5 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
 "SELECT projects.category, SUM(pledges.amount) FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
-WHERE projects.category = 'books';
+WHERE projects.category = 'books';"
 end
