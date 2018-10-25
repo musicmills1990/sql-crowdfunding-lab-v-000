42d51cece0a42a413365620826091a09abf64767
Marc Mills <mdmills1990@gmail.com>
2018-10-25 00:08:38 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,10 @@
 CREATE TABLE projects (
 id INTEGER PRIMARY KEY,
 title TEXT,
-category TEXT, 
-funding_goal INTEGER, 
-start_date TEXT, 
-end_date TEXT  
+category TEXT,
+funding_goal INTEGER,
+start_date TEXT,
+end_date TEXT
 );
 CREATE TABLE users (
 id INTEGER PRIMARY KEY,
