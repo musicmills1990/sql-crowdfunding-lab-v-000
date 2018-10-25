4a9380a7be6cb4e0b8005c32598b92b43e369771
Marc Mills <mdmills1990@gmail.com>
2018-10-25 00:48:26 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 INNER JOIN pledges
 ON pledges.project_id = projects.id
 WHERE amount_over >= 0
-GROUP BY pledges.project_id 
+GROUP BY pledges.project_id
 ;
 "
 end
