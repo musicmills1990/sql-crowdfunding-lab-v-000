65c639a9565492f751446a4fb3fbb1a14d418131
Marc Mills <mdmills1990@gmail.com>
2018-10-25 00:43:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,15 +24,17 @@ ORDER BY Users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(Pledges.amount) - projects.funding_goal AS 'amount_over'
- "
+"SELECT projects.title, SUM(Pledges.amount) - projects.funding_goal AS 'amount_over' FROM projects
+INNER JOIN pledges
+ON pledges.project_id = projects.id
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT Users.name, SUM(Pledges.amount) FROM Users
 INNTER JOIN pledges
 ON pledges.user_id = users.id
-GROUP BY pledges.user_id 
+GROUP BY pledges.user_id
 ORDER BY pledges.amount ASC;"
 end
 
