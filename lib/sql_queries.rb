db9401ed6b712d7691ed87560248fac8dbd1ee96
Marc Mills <mdmills1990@gmail.com>
2018-10-25 00:38:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT Users.name, SUM(Pledges.amount) FROM users INNTER JOIN pledges ON pledges.user_id = users.id GROUP BY pledges.user_id ORDER BY pledges.amount ASC;"
+"SELECT Users.name, SUM(Pledges.amount) FROM users 
+INNTER JOIN pledges 
+ON pledges.user_id = users.id 
+GROUP BY pledges.user_id 
+ORDER BY pledges.amount ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
