ec096dcb788f6265efad6d26327c962839e3e759
Marc Mills <mdmills1990@gmail.com>
2018-10-25 00:51:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 INNTER JOIN pledges
 ON pledges.user_id = users.id
 GROUP BY pledges.user_id
-ORDER BY pledges.amount ASC;"
+ORDER BY SUM(pledges.amount) ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
